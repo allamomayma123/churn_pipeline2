63cf4fc2cafcbfc266500c4f5738448e17ae7f49
allamomayma123 <127035970+allamomayma123@users.noreply.github.com>
2024-07-23 18:30:14 +0100

    Update score_model.py

--- src/score_model.py
+++ src/score_model.py
@@ -3,6 +3,9 @@ import pandas as pd
 from mlflow.tracking import MlflowClient
 
 def score_model(data_path):
+    """
+    Cette fonction fait le calcul des scores.
+    """
     data = pd.read_csv(data_path)
     client = MlflowClient()
     model = mlflow.sklearn.load_model(f"models:/ChurnModel/Production")
